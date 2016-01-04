07d880cee6e9be5f9e8c5ed58c7ae320c5fc1d51
Alex Berger <alex@more-onion.com>
2016-01-04 01:11:01 +0100

    fix eslint glob, fix src warning, lint test

--- test/poller_test.js
+++ test/poller_test.js
@@ -1,14 +1,16 @@
+/* global require describe it*/
+
 var assert = require('assert');
 var Poller = require('../src/poller/poller');
 
-describe('Poller', function() {
+describe('Poller', function () {
   describe('constructor', function () {
     it('should be constructable with new', function () {
       var poller = new Poller();
       assert.ok(poller instanceof Poller);
     });
     it('should be constructable without new', function () {
-      var poller = Poller();
+      var poller = Poller(); // eslint-disable-line new-cap
       assert.ok(poller instanceof Poller);
     });
   });
