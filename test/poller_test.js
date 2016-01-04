5aabd41e517fa1565c0cb85127b9ea96ed36bb0c
Alex Berger <alex@more-onion.com>
2016-01-04 01:12:06 +0100

    format poller_test.js

--- test/poller_test.js
+++ test/poller_test.js
@@ -4,15 +4,16 @@ var assert = require('assert');
 var Poller = require('../src/poller/poller');
 
 describe('Poller', function () {
-  describe('constructor', function () {
-    it('should be constructable with new', function () {
-      var poller = new Poller();
-      assert.ok(poller instanceof Poller);
+    describe('constructor', function () {
+        it('should be constructable with new', function () {
+            var poller = new Poller();
+            assert.ok(poller instanceof Poller);
+        });
+        it('should be constructable without new', function () {
+            var poller = Poller(); // eslint-disable-line new-cap
+            assert.ok(poller instanceof Poller);
+        });
     });
-    it('should be constructable without new', function () {
-      var poller = Poller(); // eslint-disable-line new-cap
-      assert.ok(poller instanceof Poller);
-    });
-  });
 });
 
+// vim: set et ts=4 sw=4 :
