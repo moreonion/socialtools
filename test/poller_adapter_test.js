418e07ff82e5f2806ed3a1171c2d15a289a799eb
Alex Berger <alex@more-onion.com>
2016-01-05 13:02:21 +0100

    mv adapters in their own modules + tests

---
+++ test/poller_adapter_test.js
@@ -0,0 +1,29 @@
+/* global require describe it */
+
+var assert = require('assert');
+var DefaultAdapterFn = require('../src/poller/adapter/default');
+var NullAdapterFn = require('../src/poller/adapter/null');
+
+describe('DefaultAdapterFn', function () {
+    describe('calling', function () {
+        it('returns the identical object it receives', function () {
+            var responseJSON = { something: 'nothing' };
+            var responseText = 'Only a string';
+            assert.equal(responseText, DefaultAdapterFn(responseText)); // eslint-disable-line new-cap
+            assert.equal(responseJSON, DefaultAdapterFn(responseJSON)); // eslint-disable-line new-cap
+        });
+    });
+});
+
+describe('NullAdapterFn', function () {
+    describe('calling', function () {
+        it('always returns null', function () {
+            var responseJSON = { something: 'nothing' };
+            var responseText = 'Only a string';
+            assert.equal(null, NullAdapterFn(responseText)); // eslint-disable-line new-cap
+            assert.equal(null, NullAdapterFn(responseJSON)); // eslint-disable-line new-cap
+        });
+    });
+});
+
+// vim: set et ts=4 sw=4 :
