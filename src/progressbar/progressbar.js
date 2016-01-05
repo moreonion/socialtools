cfc2dd516fa73e05d0fd115372fc74633ab31bc7
Alex Berger <alex@more-onion.com>
2016-01-05 16:23:01 +0100

    progressbar get currentTarget() + tests

--- src/progressbar/progressbar.js
+++ src/progressbar/progressbar.js
@@ -19,10 +19,15 @@ var Poller = require('../poller/poller');
  * @constructor
  * @this {Progressbar}
  * @param {object} options - the options
- * @param {object|Poller} options.poller - The poller to use or the options for
+ * @param {Array} [targets=[ 100, 1000, 10000 ]] - the targets
+ * @param {number} [minDelta=0] - the threshhold when the next target should be used
+ * @param {object|Poller} [options.poller] - The poller to use or the options for
  *     the poller. If the the value is of instance <code>Poller</code> it is used
  *     as-is; otherwise if it's a plain dictionary it is used as config object for
  *     the Poller instance which will get created.
+ *
+ *     If no poller is defined or this is set to <code>null</code>, no polling
+ *     will occur.
  * @param {string} options.poller.url - the URL to poll
  * @param {number} [options.poller.pollInterval=10000] - the time between polls
  *     in milliseconds
@@ -30,16 +35,75 @@ var Poller = require('../poller/poller');
  *     which adapts the response
  * @param {function} [options.poller.handler] - a handler which will get called
  *     with every successful poll
+ * @todo validation of given options
  * @public
  */
 function Progressbar(options) {
     // make it possible to use `Progressbar()` and `new Progressbar`
     if (!(this instanceof Progressbar)) return new Progressbar(options);
 
+    /**
+     * The default settings.
+     * @var {object}
+     * @inner
+     */
     var defaults = {
+        targets: [ 100, 1000, 10000 ],
+        minDelta: 0,
         poller: null
     };
+
+    /**
+     * The active settings.
+     * @member {object} settings
+     * @instance
+     * @memberof module:progressbar/progressbar~Progressbar
+     */
     this.settings = extend(defaults, options);
+
+    /**
+     * The current count state. The targets are compared against this value.
+     * @member {number} currentCount
+     * @instance
+     * @memberof module:progressbar/progressbar~Progressbar
+     */
+    this.currentCount = 0;
 }
+
+
+/**
+ * Get the current target.
+ *
+ * Returns the current target, i.e. the number to achieve, or 0 if none is
+ * set. It will return the last target if the achievable targets are all
+ * already achieved.
+ *
+ * @method
+ * @todo If targets is a string turn it into an array
+ * @todo special case when minDelta is set?
+ * @todo change minDelta default to 1; different behaviour when 0 --> return
+ *     same target
+ * @returns {number}
+ */
+Progressbar.prototype.currentTarget = function () {
+    // return 0 if no targets are configured
+    if (this.settings.targets.length === 0) {
+        return 0;
+    }
+
+    // find all target values which are *greater* than the currentCount
+    // return the minimum of these, i.e. next target
+    var self = this;
+    var possibleTargets = this.settings.targets.filter(function (value) {
+        return value > (this + self.settings.minDelta);
+    }, this.currentCount);
+    var possibleTarget = Math.min.apply(null, possibleTargets);
+
+    if (!isNaN(possibleTarget) && isFinite(possibleTarget)) {
+        return possibleTarget;
+    } else {
+        return this.settings.targets[this.settings.targets.length - 1];
+    }
+};
 
 // vim: set et ts=4 sw=4 :
--- test/progressbar_test.js
+++ test/progressbar_test.js
@@ -1,4 +1,4 @@
-/* global require describe it */
+/* global require describe it beforeEach */
 
 var chai = require('chai');
 var assert = chai.assert;
@@ -13,6 +13,56 @@ describe('Progressbar', function () {
         it('should be constructable without new', function () {
             var progressbar = Progressbar(); // eslint-disable-line new-cap
             assert.ok(progressbar instanceof Progressbar);
+        });
+    });
+
+    describe('calculation of default targets', function () {
+        beforeEach(function () {
+            this.progressbar = new Progressbar();
+        });
+
+        it('should return 100 by default', function () {
+            assert.equal(100, this.progressbar.currentTarget());
+        });
+
+        it('should return 1000 as second target by default', function () {
+            var target = this.progressbar.settings.targets[0];
+            this.progressbar.currentCount = target + 1;
+            assert.equal(1000, this.progressbar.currentTarget());
+        });
+
+        it('should return a greater target if the exact target count is hit', function () {
+            var target = this.progressbar.settings.targets[0];
+            this.progressbar.currentCount = target;
+            assert.isBelow(target, this.progressbar.currentTarget());
+            assert.notEqual(target, this.progressbar.currentTarget());
+        });
+
+    });
+
+    describe('calculation of target', function () {
+        it('should return configurable targets', function () {
+            var progressbar = new Progressbar({
+                targets: [ 23, 42, 127 ]
+            });
+            assert.equal(23, progressbar.currentTarget());
+            progressbar.currentCount = 23;
+            assert.equal(42, progressbar.currentTarget());
+        });
+
+        it('should return the last target when possible targets are exceeded', function () {
+            var progressbar = new Progressbar({
+                targets: [ 23, 42, 127 ]
+            });
+            progressbar.currentCount = 10000;
+            assert.equal(127, progressbar.currentTarget());
+        });
+
+        it('should return 0 if no targets are set', function () {
+            var progressbar = new Progressbar({
+                targets: []
+            });
+            assert.equal(0, progressbar.currentTarget());
         });
     });
 });
