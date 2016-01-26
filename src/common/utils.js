90be17d80d354fac907cc48610b2511b86de7b54
Alex Berger <alex@more-onion.com>
2016-01-26 17:06:26 +0100

    add url query param helpers to utils + test

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,13 +1,18 @@
 /**
  * Module for utilty functions.
  *
+ * A collection of helper functions, written in functional style.
+ *
  * @module common/utils
  */
 
 /*global module */
 
 module.exports = {
-    toInteger: toInteger
+    toInteger: toInteger,
+    serializeToQueryString: serializeToQueryString,
+    parseQueryString: parseQueryString,
+    addQueryParams: addQueryParams
 };
 
 /**
@@ -31,6 +36,80 @@ function toInteger(numberOrString, base) {
     } else {
         return converted;
     }
+}
+
+/**
+ * Serializes an object as query parameter string
+ *
+ * @param {object} obj - the object to serialize
+ * @returns {object}
+ * @static
+ */
+function serializeToQueryString(obj) {
+    var str = [];
+    for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+        }
+    }
+    return str.join('&');
+}
+
+/**
+ * Parses a string as query parameters
+ *
+ * @param {string} string - the string to parse
+ * @returns {object}
+ * @static
+ */
+function parseQueryString(string) {
+    if (typeof string !== 'string') {
+        string = '';
+    }
+    var vars = string.split('&');
+    var params = {};
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (!pair[0]) {
+            continue;
+        }
+        params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+    }
+    return params;
+}
+
+/**
+ * Transforms an URL into one with additional query parameters
+ *
+ * @param {string} url - the url to transform
+ * @param {object} params - the dictionary of key-value pairs to append as
+ *     query parameters
+ * @returns {string}
+ * @static
+ * @todo make this less dependent on DOM functions like parseQueryString
+ * @todo optionally apply key transforms
+ * @todo add elements if key is an array[]
+ * @todo maybe use Array.prototype.forEach()
+ */
+function addQueryParams(url, params) {
+    if (typeof url === 'undefined') {
+        return url;
+    }
+    if (typeof params !== 'object') {
+        // nothing to do here
+        return url;
+    }
+
+    var tmpUrl = document.createElement('a');
+    tmpUrl.href = url;
+
+    // add our params to the URLs params
+    var newParams = parseQueryString(tmpUrl.search.substring(1));
+    for (param in params) { // eslint-disable-line no-undef
+        newParams[param] = params[param];  // eslint-disable-line no-undef
+    }
+    tmpUrl.search = '?' + serializeToQueryString(newParams);
+    return tmpUrl.href;
 }
 
 // vim: set et ts=4 sw=4 :
--- test/common_utils_test.js
+++ test/common_utils_test.js
@@ -4,6 +4,8 @@ var chai = require('chai');
 var assert = chai.assert;
 var utils = require('../src/common/utils');
 
+var jsdom = require('mocha-jsdom');
+
 describe('utils', function () {
     describe('toInteger()', function () {
         it('should return the same integer when called with an integer', function () {
@@ -38,6 +40,52 @@ describe('utils', function () {
             assert.equal(58, utils.toInteger('3a', 16));
             assert.equal(58, utils.toInteger('3atztz', 16));
             assert.equal(0, utils.toInteger('number5', 16));
+        });
+    });
+
+    describe('addQueryParams()', function () {
+        jsdom();
+
+        it('should return undefined when called without url', function () {
+            assert.equal(undefined, utils.addQueryParams());
+        });
+
+        it('should return the same URL when called without params', function () {
+            var url = 'https://sub.example.com';
+            assert.equal(url, utils.addQueryParams(url));
+            url = 'https://sub.example.com/';
+            assert.equal(url, utils.addQueryParams(url));
+            url = 'https://sub.example.com/?test=nothing';
+            assert.equal(url, utils.addQueryParams(url));
+        });
+
+        it('should add one param', function () {
+            var url = 'https://sub.example.com';
+            assert.equal('https://sub.example.com/?one=example', utils.addQueryParams(url, {one: 'example'}));
+        });
+
+        it('should add more than one params in order', function () {
+            var url = 'https://sub.example.com';
+            assert.equal('https://sub.example.com/?one=example&two=something&three=else', utils.addQueryParams(url, {
+                one: 'example',
+                two: 'something',
+                three: 'else'
+            }));
+        });
+
+        it('should override existing params', function () {
+            var url = 'https://sub.example.com/?one=something';
+            assert.equal('https://sub.example.com/?one=else', utils.addQueryParams(url, {
+                one: 'else'
+            }));
+        });
+
+        it('should preserve fragment identifiers', function () {
+            var url = 'https://sub.example.com/#fragment';
+            assert.equal('https://sub.example.com/?one=example&two=something#fragment', utils.addQueryParams(url, {
+                one: 'example',
+                two: 'something'
+            }));
         });
     });
 });
