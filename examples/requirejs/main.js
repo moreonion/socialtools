1b9e44263f4451f275ddbf00a98994483e8f0687
Alex Berger <alex@more-onion.com>
2016-03-09 18:06:04 +0100

    update examples

--- examples/requirejs/main.js
+++ examples/requirejs/main.js
@@ -1,28 +1,32 @@
 require.config({
-    baseUrl: '../../build',
+    baseUrl: '../../build/umd',
     config: {
     }
 });
 
 require([
-    'socialtools-full-modern',
+    'poller/poller',
+    'progressbar/progressbar',
     'common/utils'
-], function (_socialtools, _utils) {
-    // the "full" distribution
-    socialtools = _socialtools;
-    // only the utils from the build tree
-    utils = _utils;
+], function (Poller, Progressbar, utils) {
     // output for verification
-    console.log(['socialtools', socialtools]);
     console.log(['utils', utils]);
+
+    poller = new Poller();
+    console.log(['poller', poller]);
+    progressbar = new Progressbar({
+        el: '#progressbar',
+        targets: [ 100, 300, 1000, 10000 ],
+        poller: null
+    });
+    console.log(['progressbar', progressbar]);
+    progressbar.update(13);
 
     // setting up an event listener in the requirejs callback context
     document.addEventListener('polled', function (e) {
         console.log(e); // eslint-disable-line no-console
     });
 
-    // making the socialtools available in the global context
-    window.Socialtools = socialtools;
 });
 
 // vim: set et ts=4 sw=4 :
